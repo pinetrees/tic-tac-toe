c9fe4ee0cfc7ff486b1bc3d1fe112a88e168d0e9
joshua <joshua@tier27.com>
2015-04-24 00:09:00 -0400

    Extended logic to check against all rows and columns

--- js/app.js
+++ js/app.js
@@ -45,36 +45,32 @@ app.controller('MainCtrl', function ($scope) {
         //  1. Have a mark in every cell for a given row
         //  2. Have a mark in every cell for a given column
         //  3. For all i between 1 and n, with n the length of the matrix, have a mark in cell (i, i)
-        //Check the rows first
-        _.each( $scope.board, function(row) {
 
-            player_one_wins = $scope.checkTuple(row, 1);
-
-            if ( player_one_wins ) return $scope.declareWinner(1);  
-
-            player_two_wins = $scope.checkTuple(row, 2);
-
-            if ( player_two_wins ) return $scope.declareWinner(2);  
-
-        } );
-
+        //We'll start by looking through the rows
         for ( i = 0; i < $scope.specs.length; i++ ) {
+            
+            //We may have a winner. If this is the case, we'll break out.
+            if ( $scope.winner ) break;
 
+            //We'll start by taking the first row.
             row = $scope.board[i];
 
             //I'm mixing in underscore, because it's going to stop checking once it fails. I'm going to mimick the same process
             player_one_wins = $scope.checkTuple(row, 1);
-
             if ( player_one_wins ) return $scope.declareWinner(1); 
 
             //If player one doesn't have all the cells in this row, we'll check the same for player 2
             player_two_wins = $scope.checkTuple(row, 2);
-
             if ( player_two_wins ) return $scope.declareWinner(2);  
 
             //No one has won yet. Before we move to the next row, we'll check the associated column
             col = _.map($scope.board, function(row) { return row[i]; });
-            console.log(col);
+
+            player_one_wins = $scope.checkTuple(col, 1);
+            if ( player_one_wins ) return $scope.declareWinner(1); 
+
+            player_two_wins = $scope.checkTuple(col, 2);
+            if ( player_two_wins ) return $scope.declareWinner(2); 
 
         }
     }
