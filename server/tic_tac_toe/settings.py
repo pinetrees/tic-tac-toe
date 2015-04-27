6724879493d902445a94e5d250cb622b5d3871ce
joshua <joshua@tier27.com>
2015-04-26 23:15:42 -0400

    Switched to sqlite

--- server/tic_tac_toe/settings.py
+++ server/tic_tac_toe/settings.py
@@ -57,18 +57,10 @@ WSGI_APPLICATION = 'tic_tac_toe.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    #'sqlite': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #},
     'default': {
-    	'ENGINE': 	'django.db.backends.mysql',
-	    'NAME':		'tic_tac_toe',
-    	'USER': 	'tic_tac_toe',
-    	'PASSWORD':	'tic_tac_toe',
-    	'HOST':		'localhost',
-    	'PORT':		'3306'
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 # Internationalization
